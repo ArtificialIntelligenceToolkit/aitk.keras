7cde95de88c50b4bf6bd5ebd76aab6fc161ea4c7
Doug Blank <doug.blank@gmail.com>
2021-09-22 01:27:34 -0700

    Don't import datasets before needing them

--- aitk/keras/_version.py
+++ aitk/keras/_version.py
@@ -8,5 +8,5 @@
 #
 # **************************************************************
 
-version_info = (0, 1, 0)
+version_info = (0, 1, 1)
 __version__ = ".".join(map(str, version_info))
--- aitk/keras/datasets/__init__.py
+++ aitk/keras/datasets/__init__.py
@@ -1,9 +1,2 @@
 """Small NumPy datasets for debugging/testing."""
 
-from . import boston_housing
-from . import fashion_mnist
-from . import imdb
-from . import reuters
-from . import cifar100
-from . import cifar10
-from . import mnist
