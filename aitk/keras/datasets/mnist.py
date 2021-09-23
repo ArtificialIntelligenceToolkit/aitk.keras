08619e5cbee6e76174dfc34617e8b467e8c1b4f0
Doug Blank <doug.blank@gmail.com>
2021-09-22 18:33:38 -0700

    Added another URL, await load_data_async()

--- aitk/keras/datasets/mnist.py
+++ aitk/keras/datasets/mnist.py
@@ -15,9 +15,15 @@
 """MNIST handwritten digits dataset."""
 
 import numpy as np
+import os
+import io
 
 from .utils import get_file
 
+origin_folders = [
+    ('https://storage.googleapis.com/tensorflow/tf-keras-datasets/', '731c5ac602752760c8e48fbffcf8c3b850d9dc2a2aedcf2cc48468fc17b673d1'),
+    ("https://raw.githubusercontent.com/ArtificialIntelligenceToolkit/datasets/master/mnist/", None),
+]
 
 def load_data(path='mnist.npz'):
     """Loads the MNIST dataset.
@@ -65,12 +71,6 @@ def load_data(path='mnist.npz'):
       [Creative Commons Attribution-Share Alike 3.0 license.](
       https://creativecommons.org/licenses/by-sa/3.0/)
     """
-    origin_folders = [
-        ('https://storage.googleapis.com/tensorflow/tf-keras-datasets/', '731c5ac602752760c8e48fbffcf8c3b850d9dc2a2aedcf2cc48468fc17b673d1'),
-        ("https://raw.githubusercontent.com/ArtificialIntelligenceToolkit/datasets/master/mnist/", None),
-    ]
-
-    path = None
     for origin_folder, file_hash in origin_folders:
         try:
             path = get_file(
@@ -80,10 +80,72 @@ def load_data(path='mnist.npz'):
         except Exception:
             print("Failed dataset download; trying another URL...")
 
-    if path is None:
-        print("Could not load dataset")
-    else:
-        with np.load(path, allow_pickle=True) as f:
-            x_train, y_train = f['x_train'], f['y_train']
-            x_test, y_test = f['x_test'], f['y_test']
-        return (x_train, y_train), (x_test, y_test)
+        if os.path.isfile(path):
+            with np.load(path, allow_pickle=True) as f:
+                x_train, y_train = f['x_train'], f['y_train']
+                x_test, y_test = f['x_test'], f['y_test']
+            return (x_train, y_train), (x_test, y_test)
+
+async def load_data_async(path='mnist.npz'):
+    """Loads the MNIST dataset.
+
+    This is a dataset of 60,000 28x28 grayscale images of the 10 digits,
+    along with a test set of 10,000 images.
+    More info can be found at the
+    [MNIST homepage](http://yann.lecun.com/exdb/mnist/).
+
+    Args:
+      path: path where to cache the dataset locally
+        (relative to `~/.keras/datasets`).
+
+    Returns:
+      Tuple of NumPy arrays: `(x_train, y_train), (x_test, y_test)`.
+
+    **x_train**: uint8 NumPy array of grayscale image data with shapes
+      `(60000, 28, 28)`, containing the training data. Pixel values range
+      from 0 to 255.
+
+    **y_train**: uint8 NumPy array of digit labels (integers in range 0-9)
+      with shape `(60000,)` for the training data.
+
+    **x_test**: uint8 NumPy array of grayscale image data with shapes
+      (10000, 28, 28), containing the test data. Pixel values range
+      from 0 to 255.
+
+    **y_test**: uint8 NumPy array of digit labels (integers in range 0-9)
+      with shape `(10000,)` for the test data.
+
+    Example:
+
+    ```python
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    assert x_train.shape == (60000, 28, 28)
+    assert x_test.shape == (10000, 28, 28)
+    assert y_train.shape == (60000,)
+    assert y_test.shape == (10000,)
+    ```
+
+    License:
+      Yann LeCun and Corinna Cortes hold the copyright of MNIST dataset,
+      which is a derivative work from original NIST datasets.
+      MNIST dataset is made available under the terms of the
+      [Creative Commons Attribution-Share Alike 3.0 license.](
+      https://creativecommons.org/licenses/by-sa/3.0/)
+    """
+    for origin_folder, file_hash in origin_folders:
+        try:
+            print("Downloading data from %s" % (origin_folder + 'mnist.npz'))
+            import js
+            response = await js.fetch(origin_folder + 'mnist.npz')
+            fp = io.BytesIO((await response.arrayBuffer()).to_py())
+            bytes = fp.read()
+            with open(path, "wb") as fp:
+                fp.write(bytes)
+        except Exception:
+            print("Could not load dataset")
+
+        if os.path.isfile(path):
+            with np.load(path, allow_pickle=True) as f:
+                x_train, y_train = f['x_train'], f['y_train']
+                x_test, y_test = f['x_test'], f['y_test']
+            return (x_train, y_train), (x_test, y_test)
