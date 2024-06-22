920c8ec103d7a5fbd19f02538be3e004704243bd
Doug Blank <doug.blank@gmail.com>
2024-06-22 08:56:38 -0400

    Deprecated: use aitk

--- Makefile
+++
@@ -1,7 +0,0 @@
-test::
-	pytest -vvv tests
-
-upload::
-	rm -rf dist/*
-	python setup.py bdist_wheel
-	twine upload dist/*
--- setup.py
+++ setup.py
@@ -8,37 +8,16 @@
 #
 # ******************************************************
 
-"""
-aitk.keras setup
-"""
-import io
-import os
-
 import setuptools
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-# The name of the project
 name = "aitk.keras"
+version = "2.0.0"
+long_description = """
+# aitk.keras
 
-
-# Get our version
-def get_version(file, name="__version__"):
-    """Get the version of the package from the given file by
-    executing it and extracting the given `name`.
-    """
-    path = os.path.realpath(file)
-    version_ns = {}
-    with io.open(path, encoding="utf8") as f:
-        exec(f.read(), {}, version_ns)
-    return version_ns[name]
-
-
-version = get_version(os.path.join(HERE, "aitk/keras/_version.py"))
-
-with open(os.path.join(HERE, "README.md"), "r") as fh:
-    long_description = fh.read()
-
+DEPRECATED PACKAGE: use `aitk` instead.
+"""
+print(long_description)
 setup_args = dict(
     name=name,
     version=version,
@@ -47,8 +26,7 @@ setup_args = dict(
     description="A Python Keras model API",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=setuptools.find_namespace_packages(include=['aitk.*']),
-    install_requires=["setuptools", "numpy", "keras_preprocessing"],
+    install_requires=["aitk"],
     python_requires=">=3.6",
     license="BSD-3-Clause",
     platforms="Linux, Mac OS X, Windows",
